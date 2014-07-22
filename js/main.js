fa9e6c2f70997783a861021b1993a657c107e2db
Hao Zhou <royhzhou@gmail.com>
2014-07-22 15:34:51 -0400

    add location

--- js/main.js
+++ js/main.js
@@ -1,27 +1,5 @@
 var app = {
 
-    registerEvents: function() {
-        var self = this;
-        // Check of browser supports touch events...
-        if (document.documentElement.hasOwnProperty('ontouchstart')) {
-            // ... if yes: register touch event listener to change the "selected" state of the item
-            $('body').on('touchstart', 'a', function(event) {
-                $(event.target).addClass('tappable-active');
-            });
-            $('body').on('touchend', 'a', function(event) {
-                $(event.target).removeClass('tappable-active');
-            });
-        } else {
-            // ... if not: register mouse events instead
-            $('body').on('mousedown', 'a', function(event) {
-                $(event.target).addClass('tappable-active');
-            });
-            $('body').on('mouseup', 'a', function(event) {
-                $(event.target).removeClass('tappable-active');
-            });
-        }
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -30,11 +8,49 @@ var app = {
         }
     },
 
+    registerEvents: function() {
+        $(window).on('hashchange', $.proxy(this.route, this));
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(
+                message,
+                null, // callback
+                title,
+                'OK' // Button label
+            );
+        } else {
+            alert(title + ": " + message);
+        }
+    },
+
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
+    },
+
     initialize: function() {
         var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
         this.registerEvents();
         this.store = new MemoryStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
+            self.route();
         });
     }
 
